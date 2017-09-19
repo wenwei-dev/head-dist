f774b5bc0d924e594db20751399c07619475a42f
Wenwei Huang <dut.hww@gmail.com>
2017-09-19 12:03:04 +0800

    initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+sudo apt-get install dpkg-sig reprepro nginx
+sudo mkdir -p /var/www/apt-repo/
+sudo cp -r conf /var/www/apt-repo/
+sudo mkdir -p /var/www/apt-repo/incoming
+sudo chown $(id -un):$(id -gn) -R /var/www/apt-repo
+sudo cp $BASEDIR/nginx.conf /etc/nginx/sites-enabled/default
+sudo cp $BASEDIR/incoming /usr/local/bin/incoming
